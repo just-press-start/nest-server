56e259d82cab5644f1cc3fce82f61c67757292ab
doruksahin <doruksahin@github.com>
2022-04-26 00:20:26 +0300

    just press start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
+import { ProductController } from './controllers/product.controller';
+import { Product, ProductSchema } from './schemas/product.schema';
+import { ProductService } from './services/product.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost/demo'),
+            MongooseModule.forFeature([{name: Product.name, schema: ProductSchema}])],
+  controllers: [AppController, ProductController],
+  providers: [AppService, ProductService],
 })
 export class AppModule {}
