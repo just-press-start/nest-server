47a3d5b1e0a7fa2607a41285148052add3903ef9
doruksahin <doruksahin@github.com>
2022-07-07 18:54:45 +0300

    updates claimPlot endpoint naming (discuss about it later)

--- src/core/plots/plots.controller.ts
+++ src/core/plots/plots.controller.ts
@@ -1,17 +1,18 @@
 import { Body, Controller, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
-import { ApiParam } from '@nestjs/swagger';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
 import { IslandPlot } from 'src/schemas/islandPlot.schema';
 import { Plot } from 'src/schemas/plot.schema';
 import { ClaimPlotDto } from './dtos/claimPlotDto';
 import { PlotsService } from './plots.service';
 
+@ApiTags('plots')
 @Controller(
     'islands/:islandId/plots',
 )
 export class PlotsController {
     constructor(private readonly plotsService: PlotsService) { }
 
-    @Put('/:plotId')
+    @Post('/:plotId/claim-plot')
     @ApiParam({ name: 'plotId', required: true, description: 'plot object id' })
     @ApiParam({ name: 'islandId', required: true, description: 'island object id' })
     async claimPlot(@Res() response, @Param('islandId') islandId, @Param('plotId') plotId, @Body() claimPlotDto: ClaimPlotDto) {
